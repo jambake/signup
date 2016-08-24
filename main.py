8172e596c8f44c0cba24f8432ef28156a0a2364c
Jim Baker <jam.baker1217@gmail.com>
2016-08-24 12:15:46 -0500

    initial setup of signup

--- main.py
+++ main.py
@@ -1,9 +1,73 @@
 import webapp2
 import re
 
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Caesar - Rot13</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h3>
+        <a href="/">Text Rotating</a>
+    </h3>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
-        self.response.write('Hello world!')
+
+        <h1>Signup</h1>
+
+        form="""
+        <form method="post">
+            <table>
+                <tr>
+                    <td><label for="username">Username</label></td>
+                    <td>
+                        <input name="username" type="text" value="" required>
+                        <span class="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="password">Password</label></td>
+                    <td>
+                        <input name="password" type="password" required>
+                        <span class="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="verify">Verify Password</label></td>
+                    <td>
+                        <input name="verify" type="password" required>
+                        <span class="error"></span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="email">Email (optional)</label></td>
+                    <td>
+                        <input name="email" type="email" value="">
+                        <span class="error"></span>
+                    </td>
+                </tr>
+            </table>
+            <input type="submit">
+        </form>
+        """
+        response = page_header + form + page_footer
+        self.response.write(response)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
