8a913ccee03a6a6ac8c106eadf38e26408055c4f
Jim Baker <jam.baker1217@gmail.com>
2016-08-24 19:58:37 -0500

    work on error messages

--- main.py
+++ main.py
@@ -25,6 +25,19 @@ page_footer = """
 </html>
 """
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
@@ -33,28 +46,32 @@ class MainHandler(webapp2.RequestHandler):
         <form method="post">
             <table style="width:50%">
                 <tr>
-                    <td><label for="username">Username</label></td>
+                    <td>Username</td>
                     <td>
-                        <input name="username" type="text" value="" required>
+                        <input name="username" type="text">
                     </td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td><label for="password">Password</label></td>
+                    <td>Password</td>
                     <td>
-                        <input name="password" type="password" value="" required>
+                        <input name="password" type="password">
                     </td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td><label for="verify">Verify Password</label></td>
+                    <td>Verify Password</td>
                     <td>
-                        <input name="verify" type="password" value="" required>
+                        <input name="verify" type="password">
                     </td>
+                    <td></td>
                 </tr>
                 <tr>
-                    <td><label for="email">Email (optional)</label></td>
+                    <td>Email (optional)</td>
                     <td>
-                        <input name="email" type="email" value="">
+                        <input name="email" type="email">
                     </td>
+                    <td></td>
                 </tr>
             </table>
             <input type="submit">
@@ -67,30 +84,66 @@ class MainHandler(webapp2.RequestHandler):
 
     def post(self):
 
-        def valid_username(username):
-            USERNAME_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-            return USERNAME_RE.match(username)
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+        error1 = ""
+        error2 = ""
+        error3 = ""
+        error4 = ""
 
-        def valid_password(verify):
-            VERIFYPASS_RE = re.compile(r"^.{3,20}$")
-            return VERIFYPASS_RE.match(verify)
+        if not valid_username(username):
+            error1 = "That's not a valid username."
 
-        def valid_email(email):
-            EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
-            return EMAIL_RE.match(email)
+        if not valid_password(password):
+            error2 = "That wasn't a valid password."
+
+        if not password == verify:
+            error3 = "Your passwords didn't match."
+
+        if not valid_email(email):
+            error4 = "That's not a valid email."
+
+        form="""
+        <form method="post">
+            <table style="width:50%">
+                <tr>
+                    <td>Username</td>
+                    <td>
+                        <input name="username" type="text">
+                    </td>
+                    <td>%s</td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>
+                        <input name="password" type="password">
+                    </td>
+                    <td>%s</td>
+                </tr>
+                <tr>
+                    <td>Verify Password</td>
+                    <td>
+                        <input name="verify" type="password">
+                    </td>
+                    <td>%s</td>
+                </tr>
+                <tr>
+                    <td>Email (optional)</td>
+                    <td>
+                        <input name="email" type="email">
+                    </td>
+                    <td>%s</td>
+                </tr>
+            </table>
+            <input type="submit">
+        </form>
+        """ % (error1, error2, error3, error4)
 
 
-        #                 <span class="error">That&#39;s not a valid username</span>
-        #
-        #                 <span class="error"></span>
-        #             
-        #                 <span class="error">Passwords don&#39;t match</span>
-        #
-        #                 <span class="error"></span>
-        #
-        #
-        # response = page_header + form + page_footer
-        # self.response.write(response)
+        response = page_header + form + page_footer
+        self.response.write(response)
 
 
 
